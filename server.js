30516cae3c7ca4418ab50fe913787776968e391a
Noor Zahara <nzahara@uncc.edu>
2020-02-19 19:47:49 -0500

    Connected to lambda func

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 3000
+const axios = require('axios')
 
 app.use(cors())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    axios.get(' https://6mazwrrnbd.execute-api.us-east-1.amazonaws.com/default/hello-world-python')
+      .then(function (response) {
+          res.send('Hello World!')
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
