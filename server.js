9fd231ef231360a1ededb63cf488402c823c9a5e
Noor Zahara <nzahara@uncc.edu>
2020-02-25 14:52:47 -0500

    Added a div to display time

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ const cors = require('cors')
 const app = express()
 const port = 3000
 const axios = require('axios')
-
+axios.defaults.timeout = 1000;
 app.use(cors())
 
 app.get('/', (req, res) => {
-    axios.get(' https://6mazwrrnbd.execute-api.us-east-1.amazonaws.com/default/hello-world-python')
-      .then(function (response) {
-          res.send(response.data)
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-})
+  axios.get("https://6mazwrrnbd.execute-api.us-east-1.amazonaws.com/default/hello-world-python")
+  .then(function (response) {
+      res.send(response.data)
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
